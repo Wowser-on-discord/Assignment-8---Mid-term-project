060ae07ae64ab576c1a47637572bee81f85ed414
Nick V <91703275+Wowser-on-discord@users.noreply.github.com>
2023-10-15 20:18:08 -0400

    Add files via upload

---
+++ js/createapost.js
@@ -0,0 +1,11 @@
+document.getElementById("createNewButton").addEventListener("click", function () {
+    var width = 1000;
+    var height = 950;
+    var left = (window.innerWidth - width) / 2;
+    var top = (window.innerHeight - height) / 2;
+
+    var options = 'width=1000,height=1000,left=' + left + ',top=' + top + ',resizable=yes,scrollbars=yes,toolbar=no,menubar=no,location=no,status=no';
+    var popupURL = 'createapost.php';
+
+    var popupWindow = window.open(popupURL, 'PopupWindow', options);
+});
